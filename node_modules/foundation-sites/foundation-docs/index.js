616289edecf6c017d9c874bee2eb312fde5c5b20
Ruchi <ruchigoel@Ruchis-MacBook-Air.local>
2016-09-27 17:22:35 -0700

    Initial Commit

---
+++ node_modules/foundation-sites/foundation-docs/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  handlebars: require('./lib/handlebars'),
+  marked: require('./lib/marked'),
+  componentTemplate: 'node_modules/foundation-docs/templates/component.html',
+  handlebarsHelpers: 'node_modules/foundation-docs/helpers/'
+}
